023cca6c0496e64050e861310441bc82dbd2fe35
evgueniazet <zelenkoea19@gmail.com>
2023-06-29 13:00:06 +0200

    feat: add jest in project

---
+++ rs-css/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRegex: '/test/.*\\.test\\.ts$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  };
+  
